952a7000019e87596c035a1a8933be4c98f95b89
woosub <woosub0602@naver.com>
2020-09-18 16:20:38 +0900

    Edit App.js 수업 내용 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
+function Food({fav}) {
+  return (
+  <h3>I love  { fav }</h3>
+  );
+}
+
 function App() {
   // return <div className="App" />;
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="김치" />
+      <Food fav="치킨" />
+      <Food fav="감자" />
+      <Food fav="배추" />
+      <Food fav="햄버거" />
+    </div>
+    );
 }
 
 export default App;
