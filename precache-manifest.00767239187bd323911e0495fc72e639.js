fa0b375284e6c27171dc94d8fc0a868f4738908d
woosub0602 <woosub0602@naver.com>
2020-11-20 16:27:30 +0900

    Updates

---
+++ precache-manifest.00767239187bd323911e0495fc72e639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98be71fddcfd1aef2acc2bec57d51be1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "990e18779a568eff216a",
+    "url": "/movie_app_2020/static/css/main.862ab1e0.chunk.css"
+  },
+  {
+    "revision": "ca1290ec655463a20b0e",
+    "url": "/movie_app_2020/static/js/2.ad030fd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.ad030fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990e18779a568eff216a",
+    "url": "/movie_app_2020/static/js/main.d786b8b6.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
